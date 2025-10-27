87c3cb8dea29cff67c19ea79cd3404688e472b47
ZCRAFT <1149461164@qq.com>
2025-10-27 20:21:34 +0800

    Fix server crashes

--- src/main/java/net/carbonmc/graphene/mixin/tickstop/LevelMixin.java
+++ src/main/java/net/carbonmc/graphene/mixin/tickstop/LevelMixin.java
@@ -1,8 +1,6 @@
 package net.carbonmc.graphene.mixin.tickstop;
 
 import net.carbonmc.graphene.TickHelper.EntityTickHelper;
-import net.carbonmc.graphene.client.GrapheneClient;
-import net.carbonmc.graphene.config.CoolConfig;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
@@ -22,10 +20,6 @@ public abstract class LevelMixin {
     )
     private void onEntityTick(Consumer<Entity> consumer, Entity entity, CallbackInfo ci) {
         if (EntityTickHelper.shouldSkipTick(entity)) {
-            ci.cancel();
-        }
-        if (!CoolConfig.isTickStoppingEnabled()) return;
-        if (GrapheneClient.instance != null && GrapheneClient.instance.shouldSkipEntity(entity)) {
             ci.cancel();
         }
     }
